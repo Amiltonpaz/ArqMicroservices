78a6edebb98082748988b742ab05eb893e0c8a2e
Amilton Carlos da Paz Junior <amilton.paz@gmail.com>
2025-03-04 14:56:16 -0300

    Padronizada a configuração do Circuit Breaker para todas as rotas.

---
+++ mscloudgateway/src/main/java/com/portifolio/mscloudgateway/config/Resilience4JConfig.java
@@ -0,0 +1,30 @@
+package com.portifolio.mscloudgateway.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class Resilience4JConfig {
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .failureRateThreshold(50)
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                        .slidingWindowSize(10)
+                        .build())
+                .timeLimiterConfig(TimeLimiterConfig.custom()
+                        .timeoutDuration(Duration.ofSeconds(4))
+                        .build())
+                .build());
+    }
+
+}
